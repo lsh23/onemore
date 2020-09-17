aef59f71fb6db1254537e825a3095cdb90e74dac
SehyeongLee <shseoul14@gmail.com>
2020-09-17 23:31:27 +0900

    Add History Entity

---
+++ src/main/java/com/formuscle/onemore/domain/History.java
@@ -0,0 +1,26 @@
+package com.formuscle.onemore.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class History {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "training_exercise_id")
+    private TrainingExercise trainingExercise;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "muscle_target_id")
+    private MuscleTarget muscleTarget;
+
+    private Float weight;
+    private Integer times;
+    private Boolean success;
+}
