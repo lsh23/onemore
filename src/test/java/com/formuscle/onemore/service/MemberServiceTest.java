3bab2bb84c69cb0ad0b953e17ac45f6ce23cd98a
SehyeongLee <shseoul14@gmail.com>
2020-09-23 21:59:42 +0900

    Test MemberService/Repository

---
+++ src/test/java/com/formuscle/onemore/service/MemberServiceTest.java
@@ -0,0 +1,36 @@
+package com.formuscle.onemore.service;
+
+import com.formuscle.onemore.domain.Member;
+import com.formuscle.onemore.repository.MemberRepository;
+import com.formuscle.onemore.repository.MuscleTargetRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Test
+    public void member_join_test() throws Exception {
+        // Given
+        Member member = new Member();
+        member.setUsername("Sehyeong");
+
+        // When
+        Long memberId = memberService.join(member);
+
+        // Then
+        assertEquals(member,memberRepository.findOne(memberId));
+    }
+}
